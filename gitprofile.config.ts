ffb9bcc7f83311e1cc42933e2fab292429fa0ad7
iggy <iggy@pop-os.home>
2024-09-19 13:46:28 +0100

    changed default theme

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -165,7 +165,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'sunset',
+    defaultTheme: 'retro',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
