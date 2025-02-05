17ca6947190d219f71dc0f90b2d91e14a568b0fc
George Stephens <georgecampbellstephens@gmail.com>
2025-02-05 18:36:36 +0000

    Changed style to winter

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -189,7 +189,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'retro',
+    defaultTheme: 'winter',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
