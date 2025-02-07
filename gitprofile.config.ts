9e67b2011b672cbffa54c67171168a34bc434ddd
George Stephens <dga2601@gmail.com>
2025-02-07 10:11:43 +0000

    Changed theme to sunset

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -189,7 +189,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'corporate',
+    defaultTheme: 'sunset',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
