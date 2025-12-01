77c1aa60c414079a9774993ea6956289a89b825c
GeorgeCStephens <dga2601@gmail.com>
2025-12-01 15:37:23 +0000

    minor tweaks

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -55,9 +55,9 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 4, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
@@ -81,8 +81,11 @@ const CONFIG = {
       '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'Java',
     'Python',
     'Selenium',
+    'Appium',
+    'Virtuoso',
     'Eggplant',
     'Git',
     'Atlassian',
